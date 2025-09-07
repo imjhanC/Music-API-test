3597816adf3c9efdf24545d250bc4ac2098ea090
imjhanC <jhan9502@gmail.com>
2025-09-08 02:43:30 +0800

    test3

--- app.py
+++ app.py
@@ -359,7 +359,7 @@ if __name__ == "__main__":
     print("Documentation at: http://localhost:8000/docs")
     
     uvicorn.run(
-        "test1:app",
+        "app:app",
         host="0.0.0.0",
         port=8000,
         reload=True,
